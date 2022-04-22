bfdbbeb05d716015160202cd6c9f766184426d42
WhiteGarfield <78983069+xXM0d3reXx@users.noreply.github.com>
2022-04-23 00:41:06 +0200

    Add files via upload

---
+++ index.js
@@ -0,0 +1,59 @@
+// INPUT
+const Discord = require('discord.js')
+const fs = require('fs')
+const mongoose = require('mongoose')
+const dotenv = require('dotenv')
+dotenv.config()
+
+//INTENTS AND PARTIALS
+const {
+    Intents
+} = Discord
+
+const client = new Discord.Client({
+
+    intents: [
+        Intents.FLAGS.GUILDS,
+        Intents.FLAGS.GUILD_MESSAGES,
+        Intents.FLAGS.GUILD_MESSAGE_REACTIONS,
+        Intents.FLAGS.GUILD_MEMBERS,
+        Intents.FLAGS.GUILD_PRESENCES,
+        Intents.FLAGS.DIRECT_MESSAGES,
+        Intents.FLAGS.DIRECT_MESSAGE_TYPING,
+        Intents.FLAGS.GUILD_VOICE_STATES
+    ],
+
+    partials: ["CHANNEL"],
+
+});
+
+// EXPORT CLIENT
+module.exports.Client = client
+
+// HANDLER
+const handlerFiles = fs.readdirSync("./handler").filter(file => file.endsWith(".js"));
+for (const file of handlerFiles) {
+    require(`./handler/${file}`);
+
+};
+console.log("Es wurden alle Handler geladen");
+
+let listing = [];
+fs.readdirSync('./handler').forEach(function (listings) {
+    listing.push(listings);
+});
+console.log(listing);
+
+client.countings = new Discord.Collection();
+const countingFiles = fs.readdirSync('./counting').filter(file => file.endsWith('.js'));
+for (const file of countingFiles) {
+    const counting = require(`./counting/${file}`);
+
+    if (counting) {
+        client.countings.set(counting.name, counting);
+    }
+};
+
+// LOGIN
+client.login(process.env.TOKEN)
+mongoose.connect(process.env.MONGO, { useNewUrlParser: true, useUnifiedTopology: true, })
