a26c6c0363dfa04a3d4564abc8d2efcd748bacee
xXM0d3reXx <78983069+xXM0d3reXx@users.noreply.github.com>
2022-04-28 17:46:04 +0200

    testing on slash commands

---
+++ handler/slashcommands.js
@@ -0,0 +1,37 @@
+const { REST } = require('@discordjs/rest');
+const { Routes } = require('discord-api-types/v9');
+const dotenv = require('dotenv')
+dotenv.config()
+const fs = require('node:fs');
+
+// Place your client and guild ids here
+const clientId = '925896859829035048';
+const guildId = '851071074736144415';
+
+const commands = [];
+const commandFolders = fs.readdirSync('./commands')
+for (const folder of commandFolders) {
+    const commandFiles = fs.readdirSync(`./commands/${folder}`).filter(file => file.endsWith('.js'))
+
+    for (const file of commandFiles) {
+        const command = require(`./commands/${file}`);
+        commands.push(command.data.toJSON());
+    }
+}
+
+const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+(async () => {
+    try {
+        console.log('Started refreshing application (/) commands.');
+
+        await rest.put(
+            Routes.applicationGuildCommands(clientId, guildId),
+            { body: commands },
+        );
+
+        console.log('Successfully reloaded application (/) commands.');
+    } catch (error) {
+        console.error(error);
+    }
+})();
