db56e9ac97a9e9913f6156cbbe6fecc17f17c540
xXM0d3reXx <78983069+xXM0d3reXx@users.noreply.github.com>
2022-04-28 18:07:41 +0200

    testing

--- handler/slashcommands.js
+++ handler/slashcommands.js
@@ -3,33 +3,35 @@ const { Routes } = require('discord-api-types/v9');
 const dotenv = require('dotenv')
 dotenv.config()
 const fs = require('node:fs');
-
+const client = require("../index").Client
 // Place your client and guild ids here
-const clientId = '925896859829035048';
-const guildId = '851071074736144415';
+client.on("ready", async () => {
+    const clientId = '925896859829035048';
+    const guildId = '851071074736144415';
 
-const commands = [];
-const commandFiles = fs.readdirSync('./testing').filter(file => file.endsWith('.js'));
+    const commands = [];
+    const commandFiles = fs.readdirSync('./testing').filter(file => file.endsWith('.js'));
 
-for (const file of commandFiles) {
-    const command = require(`./testing/${file}`);
-    commands.push(command.data.toJSON());
-}
-
-
-const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
-
-(async () => {
-    try {
-        console.log('Started refreshing application (/) commands.');
-
-        await rest.put(
-            Routes.applicationGuildCommands(clientId, guildId),
-            { body: commands },
-        );
-
-        console.log('Successfully reloaded application (/) commands.');
-    } catch (error) {
-        console.error(error);
+    for (const file of commandFiles) {
+        const command = require(`./testing/${file}`);
+        commands.push(command.data.toJSON());
     }
-})();
+
+
+    const rest = new REST({ version: '9' }).setToken(process.env.TOKEN);
+
+    (async () => {
+        try {
+            console.log('Started refreshing application (/) commands.');
+
+            await rest.put(
+                Routes.applicationGuildCommands(clientId, guildId),
+                { body: commands },
+            );
+
+            console.log('Successfully reloaded application (/) commands.');
+        } catch (error) {
+            console.error(error);
+        }
+    })();
+})
