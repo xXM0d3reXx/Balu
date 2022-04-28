141d4512fd011cd44ba7a809cb494804a3a56e1d
xXM0d3reXx <78983069+xXM0d3reXx@users.noreply.github.com>
2022-04-28 18:41:25 +0200

    testing

--- testing/echo.js
+++ testing/echo.js
@@ -1,11 +1,10 @@
 const { SlashCommandBuilder } = require('@discordjs/builders');
-module.exports = {
-    ...new SlashCommandBuilder()
-        .setName('echo')
-        .setDescription('Replies with your input!')
-        .addStringOption(option =>
-            option
-                .setName('message')
-                .setDescription('The message to echo back')
-                .setRequired(true))
-}
+
+const data = new SlashCommandBuilder()
+    .setName('echo')
+    .setDescription('Replies with your input!')
+    .addStringOption(option =>
+        option
+            .setName('message')
+            .setDescription('The message to echo back')
+            .setRequired(true))
